473f15e33c3c6cb4d07573633ab1813027b0c723
Quang Tran <quang.tran@example.com>
2026-05-08 09:21:28 +0700

    Revert headless to True for GitHub Actions

--- vga_scraper.py
+++ vga_scraper.py
@@ -190,7 +190,7 @@ class VGAScraper:
 
     async def async_run(self):
         async with async_playwright() as p:
-            browser = await p.chromium.launch(headless=False)
+            browser = await p.chromium.launch(headless=True)
             context = await browser.new_context(
                 user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
             )
